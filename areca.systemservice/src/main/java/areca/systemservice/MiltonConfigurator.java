336da7edcfbf4d3a52f942d19028ff1eaa298d97
falko <falko@delli>
2020-05-18 13:41:02 +0200

    #9: New module areca.systemservice (http://github.com/fb71/areca/issues/9)
    New modules created
    
      - app.war: compiles and builds for parent project
      - contains TeaVM compiled app and milton servlet

---
+++ areca.systemservice/src/main/java/areca/systemservice/MiltonConfigurator.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2020, the @authors. All rights reserved.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+package areca.systemservice;
+
+import javax.servlet.ServletException;
+
+import io.milton.http.HttpManager;
+import io.milton.servlet.Config;
+import io.milton.servlet.DefaultMiltonConfigurator;
+
+/**
+ *
+ * @author Falko Bräutigam
+ */
+public class MiltonConfigurator
+        extends DefaultMiltonConfigurator {
+
+    @Override
+    protected void build() {
+        // XXX Auto-generated method stub
+        throw new RuntimeException( "not yet implemented." );
+    }
+
+    @Override
+    public HttpManager configure( Config config ) throws ServletException {
+        // XXX Auto-generated method stub
+        throw new RuntimeException( "not yet implemented." );
+    }
+
+}
