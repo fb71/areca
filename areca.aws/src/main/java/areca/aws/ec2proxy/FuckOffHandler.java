87887431edfeca1c01aaac141e47faaace44b1d6
falko <falko@delli>
2023-11-10 18:02:46 +0100

    [#64]: [AWS] Loading page (http://github.com/fb71/areca/issues/64)

--- areca.aws/src/main/java/areca/aws/ec2proxy/FuckOffHandler.java
+++ areca.aws/src/main/java/areca/aws/ec2proxy/FuckOffHandler.java
@@ -35,7 +35,7 @@ public class FuckOffHandler
     private static final XLogger LOG = XLogger.get( FuckOffHandler.class );
 
     public static List<String> RULES = Arrays.asList(
-            "/.well-known*",
+            //"/.well-known*",
             "/joomla*",
             "*webalizer*",
             "*/.env",
